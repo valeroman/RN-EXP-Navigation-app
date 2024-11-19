85924affb326d3f179ed0f551b68a3b93996f4fc
valeroman <valeroman@gmail.com>
2024-11-19 06:10:39 -0500

    10-listadoproducto-detalles

---
+++ app/(stack)/products/[id].tsx
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import { Redirect, useLocalSearchParams } from 'expo-router';
+import { products } from '@/store/products.store';
+
+
+const ProductScreen = () => {
+
+    const { id } = useLocalSearchParams();
+
+    const product = products.find( p => p.id === id );
+
+    if ( !product ) {
+        return <Redirect href='/' />
+    }
+
+    return (
+        <View className='px-5 mt-10'>
+            <Text className='font-work-black text-2xl'>{ product.title }</Text>
+            <Text className=''>{ product.description }</Text>
+            <Text className='font-work-black'>{ product.price }</Text>
+        </View>
+    )
+}
+
+export default ProductScreen;
