3060e8b01c13f86ba59731570213346a609246e0
valeroman <valeroman@gmail.com>
2024-11-21 14:55:34 -0500

    18-cambiar-titulo-argumento

--- app/(drawer)/(tabs)/(stack)/products/[id].tsx
+++ app/(drawer)/(tabs)/(stack)/products/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
-import React from 'react';
-import { Redirect, useLocalSearchParams } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Redirect, useLocalSearchParams, useNavigation } from 'expo-router';
 import { products } from '@/store/products.store';
 
 
@@ -8,7 +8,15 @@ const ProductScreen = () => {
 
     const { id } = useLocalSearchParams();
 
+    const navigation = useNavigation();
+
     const product = products.find( p => p.id === id );
+
+    useEffect(() => {
+        navigation.setOptions({
+            title: product?.title ?? 'Producto'
+        })
+    },[product]);
 
     if ( !product ) {
         return <Redirect href='/' />
